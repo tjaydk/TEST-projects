edf60d224e13422f7a2d3f331dd84c9459c8f518
Emil Rosenius Pedersen <info@emilrosenius.com>
2017-02-10 08:14:21 +0100

    refactored to achieve a better cyclomatic complexity

--- Triangles/Triangles.cs
+++ Triangles/Triangles.cs
@@ -4,51 +4,77 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Triangles {
-    public class Triangles {
+namespace Triangles
+{
+    public class Triangles
+    {
 
-        public static int getTriangleType(int a, int b, int c) {
+        public static int getTriangleType(int a, int b, int c)
+        {
 
-            // Check if the inputs are valid
-            if (a < 1 || b < 1 || c < 1) {
+            if (isCandidateSides(a, b, c))
+            {
                 return 0;
             }
 
-            // Check if triangle is equilateral
-            if (a == b && c == b) {
+            if (isEquilateral(a, b, c))
+            {
                 return 1;
             }
 
-            // Check if triangle is isosceles
-            if ((a == b && c < (a * 2)) || (a == c && b < (a * 2)) || (b == c && a < (b * 2))) {
+            if (isIsosceles(a, b, c))
+            {
                 return 2;
             }
 
-            // If any of the sides are equal in length, and the above return statement isn't hit, it must be false.
-            if (a == b || a == c || c == b ) {
-                return 0;
-            }
+            .
 
-            // None of the sides are equal in length. Lets find the longest of the sides
-            // so we can check if it's longer than the remaining sides combined.
-            int[] sides = { a, b, c };
-            int longestSide = sides.Max();
-            int[] remainders = new int[2];
-            int count = 0;
-
-            for (int i = 0, max = sides.Length; i < max; i++) {
-                if (sides[i] != longestSide) {
-                    remainders[count] = sides[i];
-                    count++;
-                }
-            }
-
-            // Check if triangle is scelene
-            if (longestSide < remainders[0] + remainders[1]) {
+            if (isScalene(a, b, c))
+            {
                 return 3;
             }
 
             return 0;
         }
+
+        public static Boolean isEquilateral(int a, int b, int c)
+        {
+            return a == b && c == b;
+        }
+
+        public static Boolean isCandidateSides(int a, int b, int c)
+        {
+            return a > 0 || b > 0 || c > 0;
+        }
+
+        public static Boolean isIsosceles(int a, int b, int c)
+        {
+            return (a == b && c < (a * 2)) || (a == c && b < (a * 2)) || (b == c && a < (b * 2));
+        }
+
+        public static Boolean hasTwoEqualSides(int a, int b, int c)
+        {
+            return a == b || a == c || c == b;
+        }
+
+        public static Boolean isScalene(int a, int b, int c)
+        {
+            int[] sides = { a, b, c };
+            int longestSide = sides.Max();
+            int[] remainders = new int[2];
+            int count = 0;
+
+            for (int i = 0, max = sides.Length; i < max; i++)
+            {
+                if (sides[i] != longestSide)
+                {
+                    remainders[count] = sides[i];
+                    count++;
+                }
+            }
+
+            return longestSide < remainders[0] + remainders[1];
+        }
+
     }
 }
