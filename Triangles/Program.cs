fc0683ad55b853d17cc2acf2342cb67b0d19ef22
Emil Rosenius Pedersen <emil0srp@gmail.com>
2017-02-14 18:02:20 +0100

    added command line application

--- Triangles/Program.cs
+++ Triangles/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Triangles
@@ -11,12 +12,38 @@ namespace Triangles
         static void Main(string[] args)
         {
 
-            Console.WriteLine(Triangles.getTriangleType(2, 2, 2) + " - Should be 1");
-            Console.WriteLine(Triangles.getTriangleType(2, 3, 3) + " - Should be 2");
-            Console.WriteLine(Triangles.getTriangleType(2, 3, 4) + " - Should be 3");
-            Console.WriteLine("Press any key to stop the program");
-            Console.ReadKey();
+            Console.WriteLine("Welcome to the triangle testing program:");
+            Console.WriteLine();
 
+            while (true) {
+                Console.WriteLine("Enter three sides seperated by a comma (1,2,3):");
+
+                String input = Console.ReadLine();
+
+                if (ValidateInput(input)) {
+
+                    int[] values = GetArrayFromInput(input);
+                    Console.WriteLine(Triangles.getTriangleType(values[0], values[1], values[2]));
+                } else {
+                    Console.WriteLine("Invalid input");
+                }
+            }
         }
+
+        static Boolean ValidateInput(String input) {
+
+            Regex reg = new Regex("^([+-]?[0-9]{1,10}),([+-]?[0-9]{1,10}),([+-]?[0-9]{1,10})$");
+            
+            if (reg.Match(input).Success) {
+                return true;
+            }
+
+            return false;
+        }
+
+        static int[] GetArrayFromInput(String input) {
+            return Array.ConvertAll(input.Split(','), int.Parse);
+        } 
+
     }
 }
