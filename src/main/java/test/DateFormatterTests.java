90182e26e28f872e0d969177fb0b80dd3745ec79
Emil Rosenius Pedersen <emil0srp@gmail.com>
2017-03-14 10:45:30 +0100

    added test for london timezone

--- src/main/java/test/DateFormatterTests.java
+++ src/main/java/test/DateFormatterTests.java
@@ -1,49 +1,59 @@
 package test;
 
 import exception.InvalidTimezoneException;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 import testex.DateFormatter;
 
 import java.text.SimpleDateFormat;
+import java.util.Arrays;
 import java.util.Calendar;
 import java.util.Date;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
+@RunWith(Parameterized.class)
 public class DateFormatterTests {
+
+    @Parameterized.Parameter(0)
+    public String timeZone;
+
+    @Parameterized.Parameter(1)
+    public String expectedResult;
+
+    @Parameterized.Parameter(2)
+    public Class<? extends Exception> expectedException;
+
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
+    @Parameterized.Parameters
+    public static Iterable<Object> setupTestData() {
+        return Arrays.asList(new Object[][]{
+                {"Europe/Copenhagen", "01 Jan 2017 12:00 AM", null},
+                {"Europe/London", "31 Dec 2016 11:00 PM", null},
+                {"Europe/Invalid", "01 Jan 2017 12:00 AM", InvalidTimezoneException.class}
+        });
+    }
 
     @Test
     public void testGetFormattedDate() throws InvalidTimezoneException {
+
+        if (expectedException != null) {
+            thrown.expect(expectedException);
+        }
 
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd MMM yyyy hh:mm aa");
 
         Calendar calendar = Calendar.getInstance();
         calendar.set(2017, 0, 1, 0, 0);
-
         Date date = calendar.getTime();
-        String timeZone = "Europe/Copenhagen";
 
         DateFormatter dateFormatter = new DateFormatter(date, simpleDateFormat);
-
-
-        assertThat(dateFormatter.getFormattedDate(timeZone), is("01 Jan 2017 12:00 AM"));
+        assertThat(dateFormatter.getFormattedDate(timeZone), is(expectedResult));
     }
-
-    @Test(expected = InvalidTimezoneException.class)
-    public void testGetFormattedDateException() throws InvalidTimezoneException {
-
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd MMM yyyy hh:mm aa");
-
-        Calendar calendar = Calendar.getInstance();
-        calendar.set(2017, 0, 1, 12, 0);
-
-        Date date = calendar.getTime();
-        String timeZone = "Euwoop/Kopenhagun";
-
-        DateFormatter dateFormatter = new DateFormatter(date, simpleDateFormat);
-        dateFormatter.getFormattedDate(timeZone);
-
-    }
-
 }
